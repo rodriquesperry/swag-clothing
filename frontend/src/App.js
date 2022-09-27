d738bf1696e8c46e945db056c63c19bba3b241ba
Rodriques <rodriques.perry@gmail.com>
2022-09-26 20:33:05 -0500

    HomeScreen and Product Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import { Container } from 'react-bootstrap';
+
+import HomeScreen from './screens/homeScreen';
 import Header from './components/header';
 import Footer from './components/footer';
-
-import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Swag clothing!</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
