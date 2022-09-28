97254ebbab71a4a4c146408215aa78df80ffa24b
Rodriques <rodriques.perry@gmail.com>
2022-09-27 22:55:13 -0500

    backend initial setup and routes

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(4242, console.log('Server Running on port 4242'));
